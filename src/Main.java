06c867ccedf89c67ef251afe835fdd4001b563fd
Rasmus Møller <rasm937k@stud.kea.dk>
2020-09-02 15:12:38 +0200

    count lines done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("resources/test.txt"));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        System.out.println(lines);
+        reader.close();
     }
 }
